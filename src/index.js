0195e70734fa96ee6e23a0524763de52e8fb6c03
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-06 13:57:00 +0900

    데브툴 설치 및 환경설정
    - 데브툴 아이콘이 안보이나, 일단진행 후 코드 수정하자

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // 쿼리
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// devtools 세팅
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
@@ -12,6 +14,10 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        buttonPosition='"bottom-right"'
+      />
     </QueryClientProvider>
   </React.StrictMode>
 );
